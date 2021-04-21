74afee8e0583359578343df3e3c1fbd186bbad0b
Fabian Cook <hello@fabiancook.dev>
2021-04-21 17:25:46 +1200

    Update type signatures

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,34 @@
 import { VNode } from "@opennetwork/vnode";
-import { isPromise } from "iterable";
 
-export interface VNodeHookFn {
-  (node: VNode, parent?: VNode): VNode | Promise<VNode>;
+function isPromise<T = unknown>(value: unknown): value is Pick<Promise<T>, "then"> {
+  function isPromiseLike(value: unknown): value is { then?: unknown } {
+    return typeof value === "object";
+  }
+  return (
+    isPromiseLike(value) &&
+    value.then instanceof Function
+  );
 }
 
-export interface VNodeHook {
-  (node: VNode, parent?: VNode): Promise<VNode>;
+export interface VNodeHookFn<O extends VNode = VNode> {
+  (node: VNode, parent?: VNode): O | Promise<O>;
 }
 
-export function hook(fn: VNodeHookFn): VNodeHook {
+export interface VNodeHook<O extends VNode = VNode> {
+  (node: VNode, parent?: VNode): Promise<O>;
+}
 
-  async function *hookedChildren(parent: VNode, children: AsyncIterable<ReadonlyArray<VNode>>): AsyncIterable<ReadonlyArray<VNode>> {
+export function hook<O extends VNode = VNode>(fn: VNodeHookFn<O>): VNodeHook<O> {
+
+  async function *hookedChildren(parent: VNode, children: AsyncIterable<ReadonlyArray<VNode>>): O["children"] {
     for await (const updates of children) {
-      yield Object.freeze(
-        await Promise.all(
-          updates.map(child => hookFn(child, parent))
-        )
+      yield await Promise.all(
+        updates.map(child => hookFn(child, parent))
       );
     }
   }
 
-  async function hookFn(node: VNode, parent?: VNode): Promise<VNode> {
+  async function hookFn(node: VNode, parent?: VNode): Promise<O> {
     let hooked = fn(node, parent);
     if (isPromise(hooked)) {
       hooked = await hooked;
